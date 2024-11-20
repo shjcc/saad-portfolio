9c9ae49abda3c515faeca488036611745ad27b5c
Saad Hussain <19938855@students.ltu.edu.au>
2024-11-20 16:32:14 +1100

    updated the new template, bringing over files from my old portfolio test

--- saad-portfolio/src/App.tsx
+++ saad-portfolio/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/home/home.tsx";
+import Loading from "./pages/loading/loading";
+import Entry from "./pages/entry/entry";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Entry />}/>
+      <Route path="/entry" element={<Entry />} />
+      <Route path="/loading" element={<Loading />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- saad-portfolio/src/vite-env.d.ts
+++ saad-portfolio/src/vite-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+})
