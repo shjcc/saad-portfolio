b5f1e3610d73f38f9cd4797e45ec6da9e641b071
Saad Hussain <saad.shjcc@gmail.com>
2024-11-27 20:44:13 +1100

    fixed classnames for education and skills pages, updated stylining but not perfect

--- saad-portfolio/src/pages/home/skills.tsx
+++ saad-portfolio/src/pages/home/skills.tsx
@@ -1,21 +1,59 @@
 import styles from '../../styles/skills.module.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPython, faHtml5, faCss, faReact, faJava, faGitAlt, faNode} from '@fortawesome/free-brands-svg-icons';
+import {faPython, faHtml5, faCss3Alt, faReact, faJava, faGitAlt, faNode} from '@fortawesome/free-brands-svg-icons';
 
 const Skills = () => {
     return (
         <section id="skills" className={styles.skillsContainer}>
+
             <div className={styles.skillsContent}>
-                <h1 className={styles.skillsTitle}>Skills</h1>
+
+                <h1 className={styles.skillsTitle}>My Skills</h1>
+
                 <div className={styles.skillsList}>
-                    <FontAwesomeIcon icon={faPython} />
-                    <FontAwesomeIcon icon={faHtml5} />
-                    <FontAwesomeIcon icon={faCss} />
-                    <FontAwesomeIcon icon={faReact} />
-                    <FontAwesomeIcon icon={faJava} />
-                    <FontAwesomeIcon icon={faGitAlt} />
-                    <FontAwesomeIcon icon={faNode} />
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconA} icon={faPython}/>
+                        <span className={styles.skillsHover}>Python</span>
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconB} icon={faHtml5}/>
+                        <span className={styles.skillsHover}>HTML5</span>
+
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconC} icon={faCss3Alt}/>
+                        <span className={styles.skillsHover}>CSS3</span>
+
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconD} icon={faReact}/>
+                        <span className={styles.skillsHover}>React</span>
+
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconG} icon={faNode}/>
+                        <span className={styles.skillsHover}>Node.js</span>
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconE} icon={faJava}/>
+                        <span className={styles.skillsHover}>Java</span>
+
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <FontAwesomeIcon className={styles.skillsIconF} icon={faGitAlt}/>
+                        <span className={styles.skillsHover}>Git</span>
+
+                    </div>
+
+
                 </div>
             </div>
         </section>
