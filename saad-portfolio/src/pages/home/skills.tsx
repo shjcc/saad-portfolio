71c10ef48835f0e127a7c0b88beecea00dbd98ca
Saad Hussain <saad.shjcc@gmail.com>
2024-11-27 22:54:26 +1100

    added another skill for c++, might need to resize or change the icon

--- saad-portfolio/src/pages/home/skills.tsx
+++ saad-portfolio/src/pages/home/skills.tsx
@@ -1,7 +1,6 @@
 import styles from '../../styles/skills.module.css';
-
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPython, faHtml5, faCss3Alt, faReact, faJava, faGitAlt, faNode} from '@fortawesome/free-brands-svg-icons';
+import {faPython, faHtml5, faCss3Alt, faReact, faJava, faGitAlt, faNode, faCuttlefish} from '@fortawesome/free-brands-svg-icons';
 
 const Skills = () => {
     return (
@@ -15,57 +14,66 @@ const Skills = () => {
 
                     <div className={styles.skillsIcon}>
                         <a className={styles.skillsAnchor} href={"https://www.python.org/"} target="_blank">
-                        <FontAwesomeIcon className={styles.skillsIconA} icon={faPython}/>
+                            <FontAwesomeIcon className={styles.skillsIconA} icon={faPython}/>
                         </a>
                         <span className={styles.skillsHover}>Python</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
-                        <a className={styles.skillsAnchor} href={"https://developer.mozilla.org/en-US/docs/Glossary/HTML5"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconB} icon={faHtml5}/>
+                        <a className={styles.skillsAnchor} href={"https://isocpp.org/"} target="_blank">
+                            <FontAwesomeIcon className={styles.skillsIconB} icon={faCuttlefish}/>
                         </a>
-                            <span className={styles.skillsHover}>HTML5</span>
+                        <span className={styles.skillsHover}>C++</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
-                        <a className={styles.skillsAnchor} href={"https://developer.mozilla.org/en-US/docs/Web/CSS"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconC} icon={faCss3Alt}/>
+                        <a className={styles.skillsAnchor}
+                           href={"https://developer.mozilla.org/en-US/docs/Glossary/HTML5"} target="_blank">
+                            <FontAwesomeIcon className={styles.skillsIconC} icon={faHtml5}/>
                         </a>
-                            <span className={styles.skillsHover}>CSS3</span>
+                        <span className={styles.skillsHover}>HTML5</span>
+                    </div>
+
+                    <div className={styles.skillsIcon}>
+                        <a className={styles.skillsAnchor} href={"https://developer.mozilla.org/en-US/docs/Web/CSS"}
+                           target="_blank">
+                            <FontAwesomeIcon className={styles.skillsIconD} icon={faCss3Alt}/>
+                        </a>
+                        <span className={styles.skillsHover}>CSS3</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
                         <a className={styles.skillsAnchor} href={"https://react.dev/"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconD} icon={faReact}/>
+                            <FontAwesomeIcon className={styles.skillsIconE} icon={faReact}/>
                         </a>
-                            <span className={styles.skillsHover}>React</span>
+                        <span className={styles.skillsHover}>React</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
                         <a className={styles.skillsAnchor} href={"https://nodejs.org/en"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconG} icon={faNode}/>
+                            <FontAwesomeIcon className={styles.skillsIconF} icon={faNode}/>
                         </a>
-                            <span className={styles.skillsHover}>Node.js</span>
+                        <span className={styles.skillsHover}>Node.js</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
                         <a className={styles.skillsAnchor} href={"https://www.java.com/en/"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconE} icon={faJava}/>
+                            <FontAwesomeIcon className={styles.skillsIconG} icon={faJava}/>
                         </a>
-                            <span className={styles.skillsHover}>Java</span>
+                        <span className={styles.skillsHover}>Java</span>
                     </div>
 
                     <div className={styles.skillsIcon}>
                         <a className={styles.skillsAnchor} href={"https://git-scm.com/"} target="_blank">
-                            <FontAwesomeIcon className={styles.skillsIconF} icon={faGitAlt}/>
+                            <FontAwesomeIcon className={styles.skillsIconH} icon={faGitAlt}/>
                         </a>
-                            <span className={styles.skillsHover}>Git</span>
+                        <span className={styles.skillsHover}>Git</span>
                     </div>
 
 
                 </div>
             </div>
         </section>
-)
+    )
 }
 export default Skills;
