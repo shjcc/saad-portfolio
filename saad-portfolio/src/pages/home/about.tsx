cf7a5013414806bb14f69cb26936c670daf370fa
Saad Hussain <saadatoad@gmail.com>
2024-11-30 15:25:42 +1100

    styled the about section, needs animations

--- saad-portfolio/src/pages/home/about.tsx
+++ saad-portfolio/src/pages/home/about.tsx
@@ -7,11 +7,11 @@ const About = () => {
             <div className={styles.aboutContent}>
                 <h1 className={styles.aboutTitle}>About Me</h1>
                 <p className={styles.aboutDescription}>
-                    I am a recent graduate from La Trobe University, however I and committed to the philosophy of constantly educating myself.
+                    I am a recent graduate from La Trobe University, however I am a firm believer in the philosophy that education does not stop after graduation.
                     I earned a degree in Information Technology with a major in software engineering.
                     With a personal passion for web development and machine learning, the unique blend of creativity and problem-solving
                     within coding never fails to excite and motivate me go beyond my limits.
-                    When I'm not at my computer, I spend my time learning new cuisines to cook, keeping up my Spanish duolingo streak, watching movies, or enjoying music!
+                    <br/>When I'm not at my computer, I like to learn new cuisines to cook and maintain my Duolingo streak for Spanish. I am also interested in movies and music -almost too much!
                 </p>
             </div>
         </section>
