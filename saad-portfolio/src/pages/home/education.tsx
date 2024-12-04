2e99766bc92c71b6859ceb1461e72f0588dd6abe
Saad Hussain <saadatoad@gmail.com>
2024-12-04 18:47:39 +1100

    converted list to map through an array of strings with id for the later stagger animation

--- saad-portfolio/src/pages/home/education.tsx
+++ saad-portfolio/src/pages/home/education.tsx
@@ -1,5 +1,41 @@
 import styles from '../../styles/education.module.css';
 
+const eduLi = [
+    {
+        id: 0,
+        class: 'Algorithms and Data Structures',
+    },
+    {
+        id: 1,
+        class: 'UI/UX Design',
+    },
+    {
+        id: 2,
+        class: 'OOP Programming',
+
+    },
+    {
+        id: 3,
+        class: 'System Analysis and Design',
+    },
+    {
+        id: 4,
+        class: 'Mobile App Development',
+    },
+    {
+        id: 5,
+        class: 'Machine Learning',
+    },
+    {
+        id: 6,
+        class: 'Professional Software Development',
+    },
+    {
+        id: 7,
+        class: 'Data Visualization',
+    },
+];
+
 const Education = () => {
     return (
         <section id="education" className={styles.educationContainer}>
@@ -7,7 +43,7 @@ const Education = () => {
                 <h2 className={styles.educationTitle}>Education</h2>
                 <p className={styles.educationText}>
                     Bachelor of Information Technology, majoring in Software Engineering, La Trobe University
-                    <br />Graduate of 2024.
+                    <br/>Graduate of 2024.
                 </p>
                 <ul className={styles.educationAchievements}>
                     <li>Career Ready Advantage Award recipient, 2024</li>
@@ -15,17 +51,12 @@ const Education = () => {
                     <li>Top 10% of cohort in Professional Software Development</li>
                 </ul>
                 <div className={styles.educationListBox}>
-                <p className={styles.educationListTitle}>Some of my relevant courses</p>
-                <ul className={styles.educationList}>
-                    <li>Algorithms and Data Structures</li>
-                    <li>UI/UX Design</li>
-                    <li>OOP Programming</li>
-                    <li>Data Visualization</li>
-                    <li>System Analysis and Design</li>
-                    <li>Mobile App Development</li>
-                    <li>Machine Learning</li>
-                    <li>Professional Software Development</li>
-                </ul>
+                    <p className={styles.educationListTitle}>Some of my relevant courses</p>
+                    <ul className={styles.educationList}>
+                        {eduLi.map((course) => (
+                            <li key={course.id}>{course.class}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </section>
