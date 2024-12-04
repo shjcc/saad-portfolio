2ec165d026aa7f0d9f64b03ca5fefc066c3f3418
Saad Hussain <saadatoad@gmail.com>
2024-12-04 22:19:15 +1100

    done for tonight lol

--- saad-portfolio/src/pages/home/projects.tsx
+++ saad-portfolio/src/pages/home/projects.tsx
@@ -9,23 +9,16 @@ const Project = () => {
     return (
         <section id="projects" className={styles.projectsContainer}>
             <div className={styles.projectsContent}>
-                <h1>Projects</h1>
-                <p>Take a look at my projects.</p>
-                <div className={styles.projectsGifs}>
-                    <div className={styles.projectGif}>
-                        <a href="https://github.com/shjcc/Dr." target="_blank">
-                            <Gif src={GameGif} alt="Game Gif"></Gif>
-                        </a>
-                    </div>
-                    <div className={styles.projectGif}>
-                        <a href="https://github.com/shjcc/FlowerStoreApplication" target="_blank">
-                            <Gif src={MobileGif} alt="Mobile Gif"></Gif>
-                        </a>
-                    </div>
-                    <div className={styles.projectGif}>
-                        <a href="https://github.com/shjcc/CMS" target="_blank">
-                            <Gif src={WebsiteGif} alt="WebApp Gif"></Gif>
-                        </a>
+                <h1 className={styles.projectsTitle}>Projects</h1>
+                <p className={styles.projectsTitleText}>Have a look through some of my projects!</p>
+                <div className={styles.projectsGifList}>
+                    <div className={styles.ProjectGifBox}>
+                    <a className={styles.GameGif} href="https://github.com/shjcc/Dr." target="_blank">
+                        <Gif src={GameGif} alt="Game Gif"></Gif></a>
+                    <a className={styles.MobileGif} href="https://github.com/shjcc/FlowerStoreApplication" target="_blank">
+                        <Gif src={MobileGif} alt="Mobile Gif"></Gif> </a>
+                    <a className={styles.WebAppGif} href="https://github.com/shjcc/CMS" target="_blank">
+                        <Gif src={WebsiteGif} alt="WebApp Gif"></Gif> </a>
                     </div>
                 </div>
             </div>
