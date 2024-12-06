1956761b4018abd77a7d006dc3714cf826d63175
Saad Hussain <saadatoad@gmail.com>
2024-12-06 20:42:27 +1100

    VERSION 5 | finished the function for sending email, needs styling

--- saad-portfolio/src/pages/home/footer.tsx
+++ saad-portfolio/src/pages/home/footer.tsx
@@ -3,9 +3,27 @@ import emailjs from '@emailjs/browser';
 import styles from '../../styles/footer.module.css';
 
 const Footer = () => {
-    const form = useRef();
-    const sendEmail = () => {
+    const form = useRef(null);
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_n1wrsw4', 'template_vgnja1v', e.currentTarget, {
+                publicKey: 'cn8V8gjTY22xvh7_-',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+        e.currentTarget.reset()
     };
+
+
+
     return (
         <section id="footer" className={styles.footerContainer}>
             <div className={styles.footerContent}>
