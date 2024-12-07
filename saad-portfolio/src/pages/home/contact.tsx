c05f74c097478e2b55233aa0ed64d02a461b8193
Saad Hussain <saadatoad@gmail.com>
2024-12-07 19:53:21 +1100

    updated the about page and styling to include the linkedin and github icons and anchor points. need to update the navbar and add a resume somewhere lol

--- saad-portfolio/src/pages/home/contact.tsx
+++ saad-portfolio/src/pages/home/contact.tsx
@@ -1,5 +1,7 @@
 import {useRef} from "react";
 import emailjs from '@emailjs/browser';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import styles from '../../styles/contact.module.css';
 
 const Contact = () => {
@@ -23,24 +25,31 @@ const Contact = () => {
     };
 
     return (
-        <section id="footer" className={styles.footerContainer}>
-            <div className={styles.footerContent}>
-                <h2 className={styles.footerTitle}>Contact</h2>
+        <section id="contact" className={styles.contactContainer}>
+            <div className={styles.contactContent}>
+                <h2 className={styles.contactTitle}>Contact</h2>
                 <form
                     ref={form}
                     onSubmit={sendEmail}
-                    className={styles.footerForm}>
-                    <label className={styles.footerLabel}>Name*</label>
-                    <input className={styles.footerInput} type="text" placeholder="Your Name..." name="user_name" required/>
+                    className={styles.contactForm}>
+                    <label className={styles.contactLabel}>Name*</label>
+                    <input className={styles.contactInput} type="text" placeholder="Your Name..." name="user_name" required/>
 
-                    <label className={styles.footerLabel}>Email*</label>
-                    <input className={styles.footerInput} type="email" placeholder="Your Email..." name="user_email" required/>
+                    <label className={styles.contactLabel}>Email*</label>
+                    <input className={styles.contactInput} type="email" placeholder="Your Email..." name="user_email" required/>
 
-                    <label className={styles.footerLabel}>Message*</label>
-                    <textarea className={styles.footerInputMsg} placeholder="Your Message..." name="message"></textarea>
+                    <label className={styles.contactLabel}>Message*</label>
+                    <textarea className={styles.contactInputMsg} placeholder="Your Message..." name="message"></textarea>
 
-                    <button className={styles.footerBtn} type="submit">Send</button>
+                    <button className={styles.contactBtn} type="submit">Send</button>
                 </form>
+                <div className={styles.contactIconBox}>
+                    <p className={styles.contactIconText}>For more info</p>
+                    <a href="https://www.linkedin.com/in/saad-hussain-a0b9a629b/" target="_blank" className={styles.aboutIconAnchor}>
+                    <FontAwesomeIcon className={styles.contactIcon} icon={faLinkedin}/></a>
+                    <a href="https://github.com/shjcc" target="_blank" className={styles.aboutIconAnchor}>
+                    <FontAwesomeIcon className={styles.contactIcon} icon={faSquareGithub}/></a>
+                </div>
             </div>
         </section>
     );
